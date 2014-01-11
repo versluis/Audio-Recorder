96f6f39b6fbe10f090f68d46a2b4a9ebc2c45279
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-01-10 20:18:42 -0500

    Initial Commit

---
+++ AudioRecorderTests/AudioRecorderTests.m
@@ -0,0 +1,34 @@
+//
+//  AudioRecorderTests.m
+//  AudioRecorderTests
+//
+//  Created by Jay Versluis on 10/01/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AudioRecorderTests : XCTestCase
+
+@end
+
+@implementation AudioRecorderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
