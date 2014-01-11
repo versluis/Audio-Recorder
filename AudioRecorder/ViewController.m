d88eea37da4a599b4722d0601f28e08ae1ff1dc6
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-01-11 18:36:59 -0500

    yowser - it works ;-)

--- AudioRecorder/ViewController.m
+++ AudioRecorder/ViewController.m
@@ -11,6 +11,9 @@
 @interface ViewController ()
 
 @property (nonatomic, strong) AVAudioRecorder *recorder;
+@property (nonatomic, strong) AVAudioPlayer *player;
+// @property (nonatomic, strong) AVAudioSession *session;
+
 - (IBAction)recordButtonPressed:(id)sender;
 - (IBAction)playbackButtonPressed:(id)sender;
 - (IBAction)finishRecording:(id)sender;
@@ -31,51 +34,40 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (AVAudioRecorder *)recorder {
-    
-    if (!_recorder) {
-        _recorder = [[AVAudioRecorder alloc]init];
-        _recorder.delegate = self;
-    }
-    return _recorder;
-}
-
 - (IBAction)recordButtonPressed:(id)sender {
     
     // create a URL to an audio asset
     NSURL *documentsURL = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
     documentsURL = [documentsURL URLByAppendingPathComponent:@"audiofile.wav"];
     
+    // create an audio session
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+    if (session.inputAvailable) {
+        
+        NSLog(@"We can start recording!");
+        [session setActive:YES error:nil];
+        
+    } else {
+        NSLog(@"We don't have a mic to record with :-(");
+    }
+    
+    // settings for our recorder
+    NSDictionary *audioSettings = @{AVFormatIDKey: [NSNumber numberWithInt:kAudioFormatLinearPCM],
+                                    AVSampleRateKey: [NSNumber numberWithFloat:22050],
+                                    AVNumberOfChannelsKey: [NSNumber numberWithInt:1]
+                                    };
+    
     // create an audio recorder with the above URL
-    AVAudioRecorder *recorder = [[AVAudioRecorder alloc]initWithURL:documentsURL settings:nil error:nil];
-    self.recorder = recorder;
+    self.recorder = [[AVAudioRecorder alloc]initWithURL:documentsURL settings:audioSettings error:nil];
+    self.recorder.delegate = self;
+    [self.recorder prepareToRecord];
+    [self.recorder record];
     
     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"blue"]];
-    [self.recorder record];
 
 }
 
-- (void)audioPlayerDecodeErrorDidOccur:(AVAudioPlayer *)player error:(NSError *)error {
-    
-    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"green"]];
-    NSLog(@"Problem playing that sound file.");
-}
-
-- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag {
-    
-    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"green"]];
-    NSLog(@"There was a problem with that recording");
-}
-
-- (void)audioRecorderDidFinishRecording:(AVAudioRecorder *)recorder successfully:(BOOL)flag {
-    
-    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"green"]];
-}
-
-- (void)audioRecorderEncodeErrorDidOccur:(AVAudioRecorder *)recorder error:(NSError *)error {
-    
-    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"green"]];
-}
 
 - (IBAction)playbackButtonPressed:(id)sender {
     
@@ -83,17 +75,38 @@
     NSURL *documentsURL = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
     documentsURL = [documentsURL URLByAppendingPathComponent:@"audiofile.wav"];
     
+    // create a session
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setCategory:AVAudioSessionCategoryPlayback error:nil];
+    
     // create player
-    AVAudioPlayer *player = [[AVAudioPlayer alloc]initWithContentsOfURL:documentsURL error:nil];
+    self.player = [[AVAudioPlayer alloc]initWithContentsOfURL:documentsURL error:nil];
+    self.player.delegate = self;
+    [self.player prepareToPlay];
+    [self.player play];
+    
     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"blue"]];
-    [player play];
+    
 }
 
 - (IBAction)finishRecording:(id)sender {
     
     [self.recorder stop];
+    self.recorder = nil;
+    
+    // self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"green"]];
 }
 
+#pragma mark - Delegate Methods
 
+- (void)audioRecorderDidFinishRecording:(AVAudioRecorder *)recorder successfully:(BOOL)flag {
+    
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"green"]];
+}
+
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag {
+    
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"green"]];
+}
 
 @end
